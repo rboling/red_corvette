49bb35115c7b0099bfc3d44ae1f9e10bb46f932f
rboling91@gmail.com <rboling91@gmail.com>
2013-07-19 12:00:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamifyAsthma::Application.config.secret_token = 'feb7c8d5b5001dc87d1c47719d15ed6d4df4c673c40932afb23f9e35ffc977e216ede3b6b58b47e7d984351bdc3cf4f2670c67a722b6d77cad7d3ccaaadd6c60'
